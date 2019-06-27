45f3ab72e08073df4c7ddf17e13907b1fbb1dc26
leary <leary_e@qq.com>
2019-06-27 16:39:05 +0800

    实现七牛云上传

--- lib/test/ViewPickerTest.dart
+++ lib/test/ViewPickerTest.dart
@@ -1,59 +1,41 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_wan/res/LocalRes.dart';
-import 'package:image_cropper/image_cropper.dart';
-import 'package:image_picker/image_picker.dart';
+import 'package:flutter_wan/util/PictureSelectUtil.dart';
+import 'package:sy_flutter_qiniu_storage/sy_flutter_qiniu_storage.dart';
 
 class ViewPickerTest extends StatefulWidget {
+  ViewPickerTest({Key key}) : super(key: key);
+
   @override
   ViewPickerTestState createState() => new ViewPickerTestState();
 }
 
 class ViewPickerTestState extends State<ViewPickerTest> {
-  Future<File> _imageFile;
+  String filePath;
 
-  void getImage(ImageSource source) {
-    ImagePicker.pickImage(source: source).then((File value) {
-      setState(() {
-        _imageFile = _cropImage(value);
-      });
+  _onUpload() async {
+    //String token = '从服务端获取的token';
+    String filePath =
+        await PictureSelectUtil.getInstance.showSelectMenu(context);
+//        .then((filePath)  {
+    print('leary_filePath: ' + filePath);
+    String token =
+        'qd68lK_H3V2K8TTOCamgSCAwy2r6BddZKvYu3SGO:-YNVoHhTjc6wSlxLluh_hFBA2gA=:eyJzY29wZSI6InN5LWJ1Y2tldCIsImRlYWRsaW5lIjoxNTQ1OTkzNTAzLCJwZXJzaXN0ZW50T3BzIjoidmZyYW1lL2pwZy9vZmZzZXQvNy93LzQ4MC9oLzM2MHxzYXZlYXMvYzNrdFluVmphMlYwT2pFeU16UXpMbXB3Wnc9PSJ9';
+    final syStorage = new SyFlutterQiniuStorage();
+    //监听上传进度
+    syStorage.onChanged().listen((dynamic percent) {
+      print('leary_percent: ' + percent.toString());
     });
-  }
-
-  Future<File> _cropImage(File imageFile) async {
-    return await ImageCropper.cropImage(
-        sourcePath: imageFile.path,
-        ratioX: 1.0,
-        ratioY: 1.0,
-        maxHeight: 512,
-        maxWidth: 512);
-  }
-
-  Widget _previewImage() {
-    return FutureBuilder<File>(
-        future: _imageFile,
-        builder: (BuildContext context, AsyncSnapshot<File> snapshot) {
-          if (snapshot.connectionState == ConnectionState.done &&
-              snapshot.data != null) {
-            return Container(
-              width: 75.0,
-              height: 75.0,
-              child: ClipOval(
-                clipBehavior: Clip.antiAliasWithSaveLayer,
-                child: Image.file(
-                  snapshot.data,
-                  fit: BoxFit.fitWidth,
-                ),
-              ),
-            );
-          } else if (snapshot.error != null) {
-            //error
-            return Image.asset(LocalDrawable.mine_default_portrait);
-          } else {
-            return Image.asset(LocalDrawable.mine_default_portrait);
-          }
-        });
+    String key = DateTime.now().millisecondsSinceEpoch.toString() +
+        '.' +
+        filePath.split('.').last;
+    print('leary_key: ' + key);
+    //上传文件
+    bool result = await syStorage.upload(filePath, token, key);
+    print('leary_result: ' + result.toString()); //true 上传成功，false失败
+    setState(() {
+      this.filePath = filePath;
+    });
   }
 
   @override
@@ -62,53 +44,54 @@ class ViewPickerTestState extends State<ViewPickerTest> {
       appBar: AppBar(
         title: Text('Image Picker Example'),
       ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: () {
-          showModalBottomSheet(
-              context: context,
-              builder: (BuildContext context) {
-                return new Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-                    new ListTile(
-                      leading: new Icon(Icons.photo_camera),
-                      title: new Text("Camera"),
-                      onTap: () async {
-                        getImage(ImageSource.camera);
-                        Navigator.pop(context);
-                      },
-                    ),
-                    new ListTile(
-                      leading: new Icon(Icons.photo_library),
-                      title: new Text("Gallery"),
-                      onTap: () async {
-                        getImage(ImageSource.gallery);
-                        Navigator.pop(context);
-                      },
-                    ),
-                  ],
-                );
-              });
-        },
-        foregroundColor: Colors.white,
-        child: Text('点我'),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          FloatingActionButton(
+            heroTag: "btn2",
+            onPressed: () {
+              _onUpload();
+            },
+            foregroundColor: Colors.white,
+            child: Text('点我'),
+          ),
+          SizedBox(height: 10.0),
+          FloatingActionButton(
+            heroTag: 'btn3',
+            onPressed: () {
+              SyFlutterQiniuStorage.cancelUpload();
+            },
+            foregroundColor: Colors.red,
+            child: Text('停止'),
+          ),
+        ],
       ),
       body: Center(
         child: Stack(
           children: <Widget>[
+            Container(
+              width: 75.0,
+              height: 75.0,
+              child: ClipOval(
+                clipBehavior: Clip.antiAliasWithSaveLayer,
+                child: Image.asset(
+                  filePath ?? LocalDrawable.mine_default_portrait,
+                  fit: BoxFit.fitWidth,
+                ),
+              ),
+            )
             //头像
-            _previewImage(),
             //添加头像
-            Positioned(
-                bottom: 2.0,
-                right: 2.0,
-                child: GestureDetector(
-                  onTap: () => getImage(ImageSource.gallery),
-                  child: Padding(
-                      padding: const EdgeInsets.only(top: 5.0, left: 5.0),
-                      child:
-                          Image.asset('assets/images/icon_portrait_add.png')),
-                ))
+//            Positioned(
+//                bottom: 2.0,
+//                right: 2.0,
+//                child: GestureDetector(
+//                  onTap: () => getImage(ImageSource.gallery),
+//                  child: Padding(
+//                      padding: const EdgeInsets.only(top: 5.0, left: 5.0),
+//                      child:
+//                          Image.asset('assets/images/icon_portrait_add.png')),
+//                ))
           ],
         ),
       ),
@@ -118,20 +101,5 @@ class ViewPickerTestState extends State<ViewPickerTest> {
   @override
   void initState() {
     super.initState();
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-  }
-
-  @override
-  void didUpdateWidget(ViewPickerTest oldWidget) {
-    super.didUpdateWidget(oldWidget);
-  }
-
-  @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
   }
 }
